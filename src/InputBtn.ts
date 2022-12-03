9ca468f34615d86530e22179bf723f9b07969eba
kanyo33 <35948325+kanyo33@users.noreply.github.com>
2022-12-03 18:40:26 +0200

    updated list

---
+++ src/InputBtn.ts
@@ -0,0 +1,21 @@
+interface MyComponentProps {
+  text?: string;
+  showText?: boolean;
+  dataTypes?: string;
+  onChange?: void;
+  style?: any;
+  multiple?: boolean;
+  component?: any;
+}
+
+function InputBtn(props: MyComponentProps) {
+  const { text = "Upload", showText = true, dataTypes = "image/*", onChange = () => {}, style = {}, multiple = false, component = {} } = props;
+  return (
+    <label style={style} className="Upload__Input" htmlFor="media">
+      <input onChange={onChange} accept={dataTypes} type="file" multiple={multiple} name="media" id="media" />
+      {showText ? <p className="Upload__Text">{text}</p> : component}
+    </label>
+  );
+}
+
+export default InputBtn;
